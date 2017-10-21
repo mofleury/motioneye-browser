a0525a6fb269aabca300a1a9a2d78a4795cc039e
Marc-Olivier Fleury <mofleury@gmail.com>
2017-10-21 12:43:12 +0200

    html content is now served from python

--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
 import ConfigParser
+from string import Template
+
+
+def configSectionMap(conf, section):
+    dict1 = {}
+    options = conf.options(section)
+    for option in options:
+        dict1[option] = conf.get(section, option)
+    return dict1
+
 
 print "Content-type: text/html\n\n"
 
@@ -12,4 +22,11 @@ templatePath = config.get("directory", "templatePath")
 
 templateFile = open(templatePath, "r")
 
-print templateFile.read()
+template = Template(templateFile.read())
+
+parameters = {}
+parameters.update(configSectionMap(config, 'web'))
+
+parameters['message'] = 'tim'
+
+print template.substitute(parameters)
